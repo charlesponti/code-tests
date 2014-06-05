6d9db32b43d55e50c0a7a2eac25930798eee31ad
CJ Ponti <cjponti@gmail.com>
2014-06-05 20:26:36 +0100

    Refactor

--- qubit/Answer.js
+++ qubit/Answer.js
@@ -1,17 +1,5 @@
 var $ = function (selector) {
   /**
-   * @desc This variable will hold the elements that match the selector query.
-   * @type {Array}
-   */
-  var elements = [];
-
-  /**
-   * @desc Holder for value of selector length
-   * @type {number}
-   */
-  var length = selector.length;
-
-  /**
    * @desc This object holder RegExp matchers
    * @type {Object}
    */
@@ -56,56 +44,92 @@ var $ = function (selector) {
      * @param query
      * @returns {HTMLElement|NodeList|HTMLCollection}
      */
-    search: function(query) {
-      if (query[0] == '#') {
-        return document.getElementById(query.slice(1));
-      }
-      else if (query[0] == '.') {
-        return makeArray(document.getElementsByClassName(query.slice(1)));
-      }
-      else {
+    search: {
+      id: function(query) {
+        return document.getElementById(query);
+      },
+      class: function(query) {
+        return makeArray(document.getElementsByClassName(query));
+      },
+      tag: function(query) {
         return makeArray(document.getElementsByTagName(query));
       }
     }
   };
 
-  if (/#/.test(selector[0])) {
-    /* If selector is an id, return document.getElementById results */
-    elements.push(DOM.search(selector.slice(1)));
-  } else {
-    /**
-     * @desc Index to begin slice. Set to 0 by default.
-     * @type {number}
-     */
-    var sliceStart = 0;
+  /**
+   * @desc Index to begin slice. Set to 0 by default.
+   * @type {number}
+   */
+  var sliceStart = 0;
+  var selectorLength = selector.length;
 
-    /**
-     * @desc Iterate through selector searching for classes and ids
-     */
-    for (var i = 1; i < length; i++) {
-      if (matcher.id.test(selector[i]) && matcher.class.test(selector[i])) {
-        pushName(sliceStart, i);
-        sliceStart = i;
-      } else if (i == length - 1) {
-        /* Push last selector name in sel_names */
-        pushName(sliceStart, i + 1);
-      }
+  /**
+   * @desc Iterate through selector searching for classes and ids
+   */
+  for (var i = 1; i < selectorLength; i++) {
+    if (matcher.id.test(selector[i]) || matcher.class.test(selector[i])) {
+      pushName(sliceStart, i);
+      sliceStart = i;
+    } else if (i == selectorLength - 1) {
+      /* Push last selector name in sel_names */
+      pushName(sliceStart, i + 1);
     }
-
-    if (selectorNames.length > 1) {
-      var traverse_index = 0;
-      while (traverse_index != selectorNames.length) {
-        var selectorName = selectorNames[traverse_index];
-        elements = DOM.search(selectorName);
-        traverse_index++;
-      }
-    } else {
-      elements = DOM.search(selectorNames[0]);
-    }
-
-    var baseSelector = selectorNames[0][0];
   }
 
-  return elements;
+  var classSelectors = [];
+  var idSelectors = [];
+  var tagName;
+
+  while (selectorNames.length > 0) {
+    var sel = selectorNames[0];
+
+    if (matcher.class.test(sel)) {
+      classSelectors.push(sel.slice(1));
+    } else if (matcher.id.test(sel)) {
+      idSelectors.push(sel.slice(1));
+    } else {
+      tagName = selectorNames[0];
+    }
+
+    selectorNames.splice(0, 1);
+  }
+
+  /**
+   * @desc Return element
+   */
+  var base;
+  if (idSelectors.length == 1) {
+    base = DOM.search.id(idSelectors[0]);
+  } else if (idSelectors.length > 1) {
+    return [];
+  }
+
+  if (base) {
+
+    /* If base.tagName does not match tag name in selectorNames return empty array */
+    if (tagName && base.tagName.toLowerCase() != tagName) {
+      return [];
+    }
+
+    if (classSelectors.length) {
+      /**
+       * Check if base element has any
+       */
+      var hasClasses = classSelectors.map(function(klass) {
+        return (base && base.classList.contains(klass));
+      });
+
+      if (hasClasses.indexOf(false) > -1) {
+       return [];
+      } else {
+        return [base];
+      }
+    } else {
+      return [base];
+    }
+  }
+
+  return [];
 };
 
