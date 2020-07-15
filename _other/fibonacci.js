938a73986357ecf20f9bc9823f3a20fa641c4699
Charles Ponti <hackefeller@icloud.com>
2020-07-15 13:47:09 -0400

    chore: Remove fibonacci.html
    * Move logic to fibonacci.js

--- _other/fibonacci.js
+++ _other/fibonacci.js
@@ -1,52 +1,90 @@
-var count = 0;
+let count = 0;
 
 /**
  * @param {Number} n
  */
 function fib(n) {
-  count++
+  count += 1;
 
   if (n < 0) {
     return 0;
-  } else if (n === 1) {
+  } if (n === 1) {
     return 1;
-  } else {
-    return fib(n - 1) + fib(n - 2)
   }
+  return fib(n - 1) + fib(n - 2);
 }
 
-var countWithMemo = 0;
-var memo = {};
+let countWithMemo = 0;
+const memo = {};
 
 /**
  * @param {Number} n
  * @param {Number[]} memo
  */
 function fibWithMemo(n, memo) {
-  countWithMemo++
+  countWithMemo += 1;
 
-  if (n < 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  } 
+  if (n < 0) return 0;
+
+  if (n === 1) return 1;
 
   // If already in memo (already calculated), return value
-  else if (memo[n]) {  
-    return memo[n];
-  } 
-  
-  else {
-    // Get value
-    var val = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
-    
+  if (memo[n] === undefined) {
     // Assign to memo
-    memo[n] = val
-    
-    // Return value
-    return val;
+    memo[n] = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
   }
+
+  return memo[n];
 }
 
-console.log(fib(10), count + " times")
-console.log(fibWithMemo(10, memo), countWithMemo + " times called")
+console.log(fib(10), `${count} times`);
+console.log(fibWithMemo(10, memo), `${countWithMemo} times called`);
+
+// -----------------
+// Space Complexity (aka) memory usage
+// -----------------
+// There are only O(N) things that can be on call stack
+// at any one time
+function validSquare(g, r, c) {
+
+}
+
+function isAtEnd(g, r, c) {
+
+}
+
+let countPathsCount = 0;
+function countPaths(grid, row, col) {
+  countPathsCount++;
+
+  if (!validSquare(grid, row, col)) return 0;
+
+  if (!isAtEnd(grid, row, col)) return 1;
+
+  return (
+    countPaths(grid, row + 1, col)
+    + countPaths(grid, row, col + 1)
+  );
+}
+
+let countPathsCountWithMemo = 0;
+const paths = {};
+function countPaths(grid, row, col, paths) {
+  countPathsCountWithMemo++;
+
+  if (!validSquare(grid, row, col)) return 0;
+
+  if (!isAtEnd(grid, row, col)) return 1;
+
+  if (paths[row][col] === 0) {
+    paths[row][col] = (
+      countPaths(grid, row + 1, col)
+      + countPaths(grid, row, col + 1)
+    );
+  }
+
+  return paths[row][col];
+}
+
+console.log(fib(10), `${count} times`);
+console.log(fibWithMemo(10, memo), `${countWithMemo} times called`);
